c2937c72d548018246106bb1c738fd37500db1fa
pkliui <kliuiev@pm.me>
2024-12-09 00:43:43 +0200

    ENH improve dynamic class loading thru default class-module mapping and update its test

--- SkiNet/ML/utils/configs/dynamic_class_loader.py
+++ SkiNet/ML/utils/configs/dynamic_class_loader.py
@@ -1,41 +1,34 @@
 import importlib
 import inspect
 import logging
-from typing import Type, Optional
+from typing import Optional
+from SkiNet.ML.utils.configs.default.class_module_mapping import default_class_module_mapping
 
 
 class DynamicClassLoader:
     """
-    Dynamically loads a class given its name
+    Dynamically loads a class by its name using a default dictionary mapping class names to their respective locations
     """
 
-    def __init__(self, class_name: str):
+    def __init__(self, class_name: str, class_to_module_mapping: Optional[dict] = None):
         """
-        :param class_name: Name of a class
+        :param class_name: Name of a class to load dynamically
+        :param class_to_module_mapping: Dictionary mapping class names to their respective locations in modules, 
+            e.g. {"class_name": "module.where.this.class.is"}
         """
         self.class_name = class_name 
-        self.module_name = self._get_module_name(self.class_name)
-        """Module name for a specified class"""
+        self.class_to_module_mapping =  class_to_module_mapping or default_class_module_mapping()
 
-    def _get_module_name(self, class_name: str) -> str:
+    def load_the_class(self) -> Optional[object]:
         """
-        Return the module's name for a specified class
-
-        In the future this could be changed to find the location automatically
-        """
-        class_names = ["PH2Dataset"]
-        if class_name in class_names :
-            if class_name == "PH2Dataset":
-                return "SkiNet.ML.datasets.ph2_dataset"
-        else:
-            raise ValueError("The specified class not available for dynamic downloading yet. \
-                             Available classes are {class_names}")
-
-    def load(self) -> Optional[object]:
-        """
+        Load the specified class dynamically.
+        
         :return: The specified class if found, otherwise None.
         """
         try:
+            self.module_name = self.class_to_module_mapping[self.class_name]
+            """Module name for the specified class"""
+
             # first import the specified module
             imported_module = importlib.import_module(self.module_name)
             logging.debug(f"Importing {imported_module}")
@@ -51,7 +44,7 @@ class DynamicClassLoader:
             logging.debug(f"Found class {loaded_class} in file {imported_module}")
             return loaded_class
         except Exception as e:
-            logging.debug(f"Error loading module {self.module_name}: {str(e)}")
+            logging.debug(f"Error loading module  for class {self.class_name}: {str(e)}")
             if str(e) != "":
-                logging.warning(f"Error loading module {self.module_name}: {str(e)}")
+                logging.warning(f"Error loading module for class  {self.class_name}: {str(e)}")
             return None
--- Tests/ML/utils/configs/test_dynamic_class_loader.py
+++ Tests/ML/utils/configs/test_dynamic_class_loader.py
@@ -1,62 +1,76 @@
+"""
+Test DynamicClassLoader class
+"""
+
 import sys
-import pytest
 from unittest.mock import patch, MagicMock
 from SkiNet.ML.utils.configs.dynamic_class_loader import DynamicClassLoader
-from torch.utils.data import Dataset
 
 
-# set the names of the module where we expect the class to be and the class name itself
-module_name = "module.with.classtoload"
-class_name = "ClassToLoad"
 
-# create a new class with the name "class_name"
-class ClassToLoad:
-    def __len__(self):
-        return 0
-    
-
-def test_load_successful():
+def test_load_successful_default_mapping():
     """
-    Test successful dynamic loading the class by its name 
-    and module's location using DynamicClassLoader
+    Test successful dynamic loading of a class by its name using DynamicClassLoader and default class-module mapping
     """
 
-    # Explicitly set the __module__ attribute mimicking the location of the class in that module
-    ClassToLoad.__module__ = module_name
+    # Set the name of the class to load and the module where it is located
+    class_name = "MockedDataset"
+    module_name = "mocked.module"
 
-    # mock the module and create an actual class with the name "class_name" in it
-    with patch.dict("sys.modules", {module_name: MagicMock()}):
+
+    # Mock the module in sys.modules
+    with patch("SkiNet.ML.utils.configs.dynamic_class_loader.default_class_module_mapping") as mock_default_mapping, \
+         patch.dict("sys.modules", {module_name: MagicMock()}):
+
+        mapping_dict = {class_name: module_name}
+        mock_default_mapping.return_value = mapping_dict
+
+        # Create a new class with the name "class_name"
+        class MockedDataset:
+            def __len__(self):
+                return 0
+
+        # Explicitly set the __module__ attribute mimicking the location of the class in that module (for inspect.getmodule to work)
+        MockedDataset.__module__ = module_name
+
+        # Get the mocked module from sys.modules and add the class to that mocked module
         mocked_module = sys.modules[module_name]
-        setattr(mocked_module, class_name, ClassToLoad)
+        setattr(mocked_module, class_name, MockedDataset)
 
-        # Test loading the class
-        loader = DynamicClassLoader(module_name, class_name)
-        loaded_class = loader.load()
-        assert loaded_class == ClassToLoad
+        # Inject the mocked mapping into the DynamicClassLoader
+        loader = DynamicClassLoader(class_name)
+        loaded_class = loader.load_the_class()
+
+        assert loaded_class == MockedDataset
 
 
-def test_load_nonexistent_module():
+def test_load_successful_custom_mapping():
     """
-    Test loading the specified class in a non-existent module
-    """
-    loader = DynamicClassLoader("nonexistent_module", class_name)
-    loaded_class = loader.load()
-    assert loaded_class is None
-
-
-def test_load_class_not_in_module():
-    """
-    Test when the class is not actually in the module
+    Test successful dynamic loading of a class by its name using DynamicClassLoader with a custom class-to-module mapping
     """
 
-    class ClassNotInModule:
-        pass
+    # Define the custom mapping for the test
+    class_name = "CustomMockedDataset"
+    module_name = "custom.mocked.module"
+    custom_mapping = {class_name: module_name}
 
-    # mock the module but do not create any class  in it
+    # Mock the module in sys.modules
     with patch.dict("sys.modules", {module_name: MagicMock()}):
-        mocked_module = sys.modules[module_name]
+        
+        # Create a new class with the name "class_name"
+        class CustomMockedDataset:
+            def __len__(self):
+                return 0
 
-        # Test loading the class
-        loader = DynamicClassLoader(module_name, "ClassNotInModule")
-        loaded_class = loader.load()
-        assert loaded_class is None
+        # Explicitly set the __module__ attribute mimicking the location of the class in that module (for inspect.getmodule to work)
+        CustomMockedDataset.__module__ = module_name
+
+        # Get the mocked module from sys.modules and add the class to that mocked module
+        mocked_module = sys.modules[module_name]
+        setattr(mocked_module, class_name, CustomMockedDataset)
+
+        # Initialize DynamicClassLoader with the custom mapping
+        loader = DynamicClassLoader(class_name, class_to_module_mapping=custom_mapping)
+        loaded_class = loader.load_the_class()
+
+        assert loaded_class == CustomMockedDataset
