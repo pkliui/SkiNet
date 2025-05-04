f1b3eab18b15fe4bc9a36f9eea38b7a98174bad2
pkliui <kliuiev@pm.me>
2025-05-04 18:45:13 +0300

    REFACTOR enable plotting in jupyter from within docker

--- SkiNet/Plotting/get_data/get_images_and_masks.py
+++ SkiNet/Plotting/get_data/get_images_and_masks.py
@@ -45,3 +45,17 @@ def read_images_from_directory(directory_path, search_pattern, max_num_images_to
         except Exception as e:
             logging.getLogger(__name__).warning(f"Could not open image {image_path}: {e}")
     return images
+
+
+def read_paths_to_images_from_directory(directory_path, search_pattern) -> list[Path]:
+    """
+    Reads paths to all images in the directory that match the given search pattern
+
+    :param directory_path: Path to the directory containing images.
+    :param search_pattern: for example for bmp images located in a folder  having "Dermoscopic_Image" in its name '*_Dermoscopic_Image/*.bmp'
+    :return: A list of paths to images.
+    """
+    image_paths = list(Path(directory_path).rglob(search_pattern))
+    
+    return image_paths
+
