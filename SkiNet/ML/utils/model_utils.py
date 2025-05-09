cfa681f0124d3aa53bc5d6c10a9df955b09e7cce
pkliui <kliuiev@pm.me>
2025-05-09 09:01:25 +0300

    FEAT add workflow states mapping and refactor plot_segmentations

---
+++ SkiNet/ML/utils/model_utils.py
@@ -0,0 +1,35 @@
+from enum import Enum, unique
+
+
+@unique
+class MLWorkflowState(Enum):
+    """
+    Keeps ML workflow states
+    """
+    TRAIN = "Training"
+    VAL = "Validation"
+    TEST = "Testing"
+
+
+def state_mapping(input_state_name: str) -> MLWorkflowState:
+    """
+    Mapping of ML workflow states to their string representations of different spellings    
+    
+    :param input_state_name: The string representation of the state as provided by user, e.g. "train", "Train" or "training" for training dataset
+    :return: The corresponding MLWorkflowState enum value.
+    """
+
+    #   
+    state_mapping = {
+        "train": MLWorkflowState.TRAIN,
+        "training": MLWorkflowState.TRAIN,
+        "val": MLWorkflowState.VAL,
+        "validation": MLWorkflowState.VAL,
+        "test": MLWorkflowState.TEST,
+        "testing": MLWorkflowState.TEST,
+    }
+
+    state = state_mapping.get(input_state_name.lower())
+    if state is None:
+        raise ValueError(f"Invalid state name: '{input_state_name}'. Must be one of {list(state_mapping.keys())}.")
+    return state
