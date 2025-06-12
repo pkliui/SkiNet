f47acffa272f4a93cacc58125dc72f82097cb3ea
pkliui <kliuiev@pm.me>
2025-06-12 13:41:12 +0300

    FEAT add default class to module mapping for dynamic class loader

---
+++ SkiNet/ML/utils/configs/default/class_module_mapping.py
@@ -0,0 +1,7 @@
+def default_class_module_mapping():
+    """
+    Returns a dictionary mapping class names to their respective locations within the SkiNet file structure.
+    """
+    return {
+        "PH2Dataset": "SkiNet.ML.datasets.ph2dataset"
+    }
