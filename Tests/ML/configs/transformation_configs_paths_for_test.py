6923df9ca6e657049c2df8aeba045624d57e61bd
pkliui <kliuiev@pm.me>
2025-07-17 12:36:39 +0300

    REFACTOR add dedicated function to load transform from config

--- Tests/ML/configs/transformation_configs_paths_for_test.py
+++ Tests/ML/configs/transformation_configs_paths_for_test.py
@@ -2,7 +2,7 @@
 Construct transformation configs for their use in unit tests
 """
 from SkiNet.ML.configs import transformations_config
-from SkiNet.Utils.project_paths_tests import TRANSFORMATION_CONFIGS_YAML_PATH 
+
 
 # default configuration 
 # to be used in test_transform_data.py/test_make_transform_from_config_PIL_image
@@ -10,6 +10,6 @@ config_test_transform_data = transformations_config.get_default_config()
 
 # configuration that uses YAML overrriding defaults 
 # to be used in test_transform_data.py/test_make_transform_from_configYAML_PIL_image
-config_yaml_test_transform_data = transformations_config.get_default_config()
-config_yaml_test_transform_data.merge_from_file(TRANSFORMATION_CONFIGS_YAML_PATH) # override from YAML
-config_yaml_test_transform_data.freeze() #  to prevent further modification
+from SkiNet.ML.transformations.transform_utils import load_transform_config
+from SkiNet.Utils.project_paths_tests import TRANSFORMATION_CONFIGS_YAML_PATH
+config_yaml_test_transform_data = load_transform_config(TRANSFORMATION_CONFIGS_YAML_PATH)
