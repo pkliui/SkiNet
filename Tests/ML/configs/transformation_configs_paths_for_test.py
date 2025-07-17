765b62347c6d4fee316c0ecfa2bddc5119fd3136
pkliui <kliuiev@pm.me>
2025-07-17 09:35:42 +0300

    TESTS use paths for yaml config transforms from dedicated file

--- Tests/ML/configs/transformation_configs_paths_for_test.py
+++ Tests/ML/configs/transformation_configs_paths_for_test.py
@@ -4,10 +4,12 @@ Construct transformation configs for their use in unit tests
 from SkiNet.ML.configs import transformations_config
 from SkiNet.Utils.project_paths_tests import TRANSFORMATION_CONFIGS_YAML_PATH 
 
-# default configuration to be used in test_transform_data to test transform_data.make_transform_from_config 
+# default configuration 
+# to be used in test_transform_data.py/test_make_transform_from_config_PIL_image
 config_test_transform_data = transformations_config.get_default_config()
 
-# configuration that uses YAML overrriding defaults to be used in test_transform_data to test transform_data.make_transform_from_config 
+# configuration that uses YAML overrriding defaults 
+# to be used in test_transform_data.py/test_make_transform_from_configYAML_PIL_image
 config_yaml_test_transform_data = transformations_config.get_default_config()
 config_yaml_test_transform_data.merge_from_file(TRANSFORMATION_CONFIGS_YAML_PATH) # override from YAML
 config_yaml_test_transform_data.freeze() #  to prevent further modification
