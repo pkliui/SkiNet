0a87e8cb21ffebf7c75c318933024cabdc4f6c8f
pkliui <kliuiev@pm.me>
2025-07-18 15:23:45 +0300

    CHORE toggle  azure tests

--- SkiNet/Utils/test_utils/run_pytest_tests.py
+++ SkiNet/Utils/test_utils/run_pytest_tests.py
@@ -4,10 +4,16 @@ import logging
 from SkiNet.Utils import project_paths
 
 
-def run_pytest_tests():
+def run_pytest_tests(run_azure_tests: bool = False):
     """
     Run pytest tests located in project_paths.TESTS_DIR directory
+
+    :param run_azure_tests If True, also runs Azure integration tests. If False, skips them. Default is False
     """
-    pytest_args = [project_paths.TESTS_DIR,"-v", "-s"]
+    if run_azure_tests:
+        pytest_args = [project_paths.TESTS_DIR, "-v", "-s"]
+    else:
+        pytest_args = [project_paths.TESTS_DIR, "-v", "-s", "-m", "not azure"]
+    
     logging.getLogger(__name__).info("Running pytest using following arguments: %s", pytest_args)
     pytest.main(pytest_args)
