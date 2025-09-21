95c4548c3dd30b2a755f5c34bbd8ff089e5dd3ca
pkliui <kliuiev@pm.me>
2025-09-21 18:39:31 +0300

    add random number generator seeds

--- SkiNet/ML/utils/model_utils.py
+++ SkiNet/ML/utils/model_utils.py
@@ -1,4 +1,11 @@
 from enum import Enum, unique
+import inspect
+import logging
+import os
+import random
+from typing import Optional
+import numpy as np
+import torch
 
 @unique
 class MLWorkflowState(Enum):
@@ -33,3 +40,20 @@ def state_mapping(input_state_name: str) -> MLWorkflowState:
     else:
         raise ValueError(f"Invalid ML workflow state name: '{input_state_name}'. Must be one of {list(state_mapping_dict.keys())}.")
     return state
+
+
+def set_random_seed(random_seed: int, module_name: Optional[str] = None) -> None:
+    """
+    Seed for random number generators 
+
+    :param random_seed: seed value
+    :param module_name: module name where this seed is called from
+    """
+
+    if module_name is None:
+        module_name = os.path.basename(inspect.stack()[1].filename)
+        logging.getLogger(__name__).debug(f"Random seed set to {random_seed} in module {module_name}.")
+ 
+    torch.manual_seed(random_seed)
+    np.random.seed(random_seed)
+    random.seed(random_seed)
