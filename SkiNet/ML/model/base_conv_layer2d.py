ef0b1f17f05074624df9ea1dc61bb259696996d4
pkliui <kliuiev@pm.me>
2025-10-03 08:43:35 +0300

    FEAT TEST add baseconv2d with tests

--- SkiNet/ML/model/base_conv_layer2d.py
+++ SkiNet/ML/model/base_conv_layer2d.py
@@ -1,6 +1,6 @@
 import torch
 from typing import Optional, Callable, Union, Iterable
-from SkiNet.ML.utils.sampling.encoder_sampling import PaddingMode, get_padding_value
+from SkiNet.ML.utils.sampling.encoder_sampling import PaddingMode, get_padding_and_stride
 
 
 class BaseConv2D(torch.nn.Module):
@@ -10,14 +10,19 @@ class BaseConv2D(torch.nn.Module):
     :param in_channels: Number of input channels.
     :param out_channels: Number of output channels.
     :param kernel: Kernel size of the convolution operation.
-    :param stride: Stride of the convolution operation
+    :param stride: Stride of the convolution operation. Required if padding_mode is 'VALID'. Otherwise is set based on padding_mode.
+        Default is 1
     :param padding_mode: Padding mode applied to input. Should be of type PaddingMode.
         Possible values are 'VALID' (no padding applied), 'SAME' (padding applied to keep same spatial dimensions), 
-        and 'DOWNSAMPLING_FACTOR_2' (downsample the output by factor 2).
-
+        and 'DOWNSAMPLING_FACTOR_2' (downsample the output by factor 2). 
+        Default is 'SAME'.
     :param dilation: Dilation factor of the convolution operation.
-    :param bias: If True, adds a learnable bias to the output.
-    :param apply_batchnorm: If True, applies batch normalization after convolution.
+        Default is 1
+    :param bias: If True, adds a learnable bias to the output. Note that this parameter is ignored and set to False
+        when `apply_batchnorm=True` because batch normalization includes its own learnable parameters.
+        Default is False
+    :param apply_batchnorm: If True, applies batch normalization after convolution. When enabled, the `bias` parameter
+        is ignored and set to False.
     :param activation: Non-linear activation function applied after batch normalization. If None, no activation is applied.
     
     :return: Output tensor after applying convolution, optional batch normalization, and optional activation.
@@ -31,38 +36,32 @@ class BaseConv2D(torch.nn.Module):
                  dilation: Union[int, Iterable[int]] = 1,
                  bias: bool = False,
                  apply_batchnorm: bool = True,
-                 activation: Optional[Callable] = torch.nn.ReLU
-                 ):
+                 activation: Optional[Callable] = torch.nn.ReLU):
         super().__init__()
 
         if not isinstance(padding_mode, PaddingMode):
             raise ValueError(f"Invalid padding mode: {padding_mode}. Must be one of {list(PaddingMode)}.")
-        self.padding = get_padding_value(stride=stride,
-                                                 kernel=kernel,
-                                                 dilation=dilation,
-                                                 padding_mode=padding_mode,
-                                                 num_dims=2)
-        """Padding value calculated based on padding mode, kernel size, stride and dilation"""
         
+        pad_and_stride = get_padding_and_stride(kernel=kernel,
+                                                dilation=dilation,
+                                                padding_mode=padding_mode,
+                                                num_dims=2,
+                                                stride=stride)
+        self.padding, self.stride = pad_and_stride.padding, pad_and_stride.stride
+        """Padding value calculated based on padding mode, kernel size, and dilation"""
+        
+        self.apply_batchnorm = apply_batchnorm
         self.conv2d = torch.nn.Conv2d(
             in_channels=in_channels,
             out_channels=out_channels,
             kernel_size=kernel,
-            stride=stride,
+            stride=self.stride,
             padding=self.padding,
             dilation=dilation,
-            bias=bias
-        )
+            bias=not self.apply_batchnorm)  # Set bias based on apply_batchnorm
         """Conv 2d layer"""
-       
-        self.apply_batchnorm = apply_batchnorm
-        if self.apply_batchnorm:
-            self.bias = False
-            self.batchnorm2d = torch.nn.BatchNorm2d(out_channels)
-        else:
-            self.bias = True
-            self.batchnorm2d = None
-        """Bias is not used if batchnorm is applied. This is because batchnorm has its own learnable parameters that can offset the mean of the activations."""
+
+        self.batchnorm2d = torch.nn.BatchNorm2d(out_channels) if self.apply_batchnorm else None
         """Batchnorm layer"""
 
         self.activation = activation(inplace=True) if activation else None
